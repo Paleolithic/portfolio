85e085dc79a9516f42854b9b4451f5c63e6e437c
root <root@TJ-Linux.(none)>
2013-08-18 17:09:32 -0400

    Added progress bar, made it animate on button click, reset on other button clicks

--- js/progressbar.js
+++ js/progressbar.js
@@ -11,5 +11,28 @@
  */
 function progressBar(percent, $element, caption) {
 	var progressBarWidth = percent * $element.width() / 100;
-	$element.find('div').animate({ width: progressBarWidth }, 500).html(caption + percent + "%&nbsp;");
+	$element.find('div').animate({ width: progressBarWidth }, 500).html(caption/** + percent*/ +"&nbsp;");
 }
+
+var progressBars = new Array();
+progressBars[0] = "setTimeout(function(){progressBar(50, $('#college'), '50%');},1500); "
+
+$(document).ready(function(){
+    $("#about_me").click(function(){
+	  	setTimeout(function(){progressBar(50, $('#college'), '50%');},1500); 
+		setTimeout(function(){progressBar(50, $('#tc'), '2 years');},1700); 
+		setTimeout(function(){progressBar(25, $('#notes'), '1 year');},1900); 
+		setTimeout(function(){progressBar(25, $('#rc'), '1 year');},2100); 
+		setTimeout(function(){progressBar(30, $('#life'), '30%');},2300);
+  	});
+
+
+    $(".unload").click(function(){
+	  	setTimeout(function(){progressBar(0, $('#college'), '');},300); 
+		setTimeout(function(){progressBar(0, $('#tc'), '');},300); 
+		setTimeout(function(){progressBar(0, $('#notes'), '');},300); 
+		setTimeout(function(){progressBar(0, $('#rc'), '');},300); 
+		setTimeout(function(){progressBar(0, $('#life'), '');},300);
+  	});
+
+});
